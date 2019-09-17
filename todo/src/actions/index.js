b95d528c9f2f8d0a706e909c7032fe4a673a4eb4
bridgelancer <thchoy@hotmail.com>
2019-09-17 15:13:06 +0800

    Establish necessary redux functionalities
    -   ADD_TODO, TOGGLE_TODO rudimentarily tested with index.js

---
+++ todo/src/actions/index.js
@@ -0,0 +1,15 @@
+let nextId = 0;
+
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+
+export const addTodo = text => ({
+    type: ADD_TODO,
+    id: nextId++,
+    text
+})
+
+export const toggleTodo = id => ({
+    type: TOGGLE_TODO,
+    id
+})
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import App from './components/App';
+import todoApp from './reducers/'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import {
+  addTodo,
+  toggleTodo,
+} from './actions'
+const store = createStore(todoApp);
+
+console.log(store.getState())
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
+
+// Dispatch some actions
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+
+// Stop listening to state updates
+unsubscribe()
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
