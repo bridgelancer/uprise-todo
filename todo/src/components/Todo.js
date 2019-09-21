ec8aaa63bbb3b7dd73bf0166277fce00a6ffba5d
bridgelancer <thchoy@hotmail.com>
2019-09-21 17:24:08 +0800

    Move toggle to button

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Button from '@material-ui/core/Button';
 
 function Todo({ id, todo_text, onClick, completed } ){
-  console.log('completed: ', completed)
+
   return (
     <li
-      onClick={onClick}
       style={{
         textDecoration: completed ? 'line-through' : 'none'
       }}
     >
     {id}, {todo_text}, {completed.toString()}
+    <Button variant='contained' size='small'
+            onClick={onClick}>
+      Toggle
+    </Button>
+
     </li>
   )
 }
