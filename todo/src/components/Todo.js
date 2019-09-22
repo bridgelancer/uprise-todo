c89a7d75dc9883bf9d4aed16f919ce381a87bb97
bridgelancer <thchoy@hotmail.com>
2019-09-22 16:12:58 +0800

    Convert vanilla list ot MaterialUI table

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import EditTodo from '../containers/EditForm';
+
+import Button from '@material-ui/core/Button';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 function Todo({ id, todo_text, onToggle, onEdit, completed } ){
 
   return (
-    <li
-      style={{
-        textDecoration: completed ? 'line-through' : 'none'
-      }}
-    >
-    {id}, {todo_text}, {completed.toString()}
+    <>
+    <TableCell>{id}</TableCell>
+    <TableCell>{todo_text}</TableCell>
+    <TableCell>{completed.toString()}</TableCell>
+
     <Button variant='contained' size='small'
             onClick={onToggle}>
       Toggle
     </Button>
 
     <EditTodo onEdit={onEdit}/>
-    </li>
+    </>
   )
 }
 
--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import Todo from './Todo'
 
-function TodoList({todos, toggleTodo, editTodo}) {
-  return (
-      todos.map(todo =>
-        <ul key={todo.id}>
-          <Todo
-          key={todo.id}
-          {...todo}
-          onToggle={() => toggleTodo(todo.id)}
-          onEdit={(text) => editTodo(todo.id, text)}
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
-          />
-        </ul>
-      )
+function TodoList({todos, toggleTodo, editTodo}) {
+  const rows = todos.map(todo => (<Todo
+                        key={todo.id}
+                        {...todo}
+                        onToggle={() => toggleTodo(todo.id)}
+                        onEdit={(text) => editTodo(todo.id, text)}
+                      />
+                  )
+              )
+  console.log(rows)
+  return (
+      <Table>
+        <TableHead>
+            <TableRow>
+              <TableCell>id</TableCell>
+              <TableCell>Text</TableCell>
+              <TableCell>Completed</TableCell>
+            </TableRow>
+        </TableHead>
+
+        <TableBody>
+            {rows.map((currentTodo) => (<TableRow> {currentTodo} </TableRow>))}
+        </TableBody>
+      </Table>
   )
 }
 
